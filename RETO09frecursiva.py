72e338525e7c514feb5657d58b0ba7d755c07cfb
Juansemg2 <juamartinezga@unal.edu.co>
2025-07-01 20:12:25 -0500

    Add files via upload

---
+++ RETO09frecursiva.py
@@ -0,0 +1,9 @@
+# Función recursiva para calcular la potencia
+def potencia(base: int, exponente: int) -> int:
+    if exponente == 0:
+        return 1
+    else:
+        return base * potencia(base, exponente - 1)
+
+if __name__ == "__main__":
+    print("2^4 =", potencia(2, 4))
